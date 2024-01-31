0d50664decd42e2d4b6e752ab1e7911159f87d4f
Rafael Fourtra <rafael44842@gmail.com>
2024-01-31 22:50:28 +0700

    rafa 31 jan last

--- app/layout.js
+++ app/layout.js
@@ -15,7 +15,6 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
-         <Providers>  
          <Providers>
             <Navbar />
             <FloatingButton />
