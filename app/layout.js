ebebe41600f2660e7554cd38b391219e17cad8cd
Rafael Fourtra <rafael44842@gmail.com>
2024-01-31 22:40:54 +0700

    fixing

--- app/layout.js
+++ app/layout.js
@@ -12,7 +12,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
-         <Providers>
+         <Providers>  
             {children}
          </Providers>
       </body>
--- app/tema-filter/section2.js
+++ app/tema-filter/section2.js
@@ -1,3 +1,6 @@
+'use client'
+ 
+import { useState } from 'react'
 // Component
 import { Button } from "@nextui-org/react";
 
@@ -7,32 +10,43 @@ import { FaArrowRight } from "react-icons/fa";
 import { MdOutlineShoppingCartCheckout } from "react-icons/md";
 
 const SectionTwo = () => {
+
+    const [selectedId, setSelectedId] = useState(1);
+
+      const handleButtonClick = (id) => {
+        setSelectedId(id);
+      };
+
+
+    const list = [
+        {
+          title: "Undangan Digital",
+          id: 1
+        },
+        {
+          title: "Undangan 3D",
+          id: 2
+        },
+        {
+          title: "Filter Instagram",
+          id: 3
+        },
+      ];
+
     return (
-        <div className="p-10 pt-16">
-            <h1 className="text-center text-[#221C35] text-4xl font-semibold ">Pilihan Tema & Filter Instagram</h1>
-            <div className="flex items-center justify-center">
-                <div className="grid grid-cols-4 w-9/12 pt-7">
-                    <div className="flex items-center justify-center">
-                        <Button className="font-semibold text-slate-500" size="md" radius="full">Undangan Video <FaCaretDown /></Button>
-                    </div>
-                    <div className="flex items-center justify-center">
-                        <Button className="font-semibold text-white bg-[#221C35]" size="md" radius="full">Wedding Invitation <FaCaretDown /></Button>
-                    </div>
-                    <div className="flex items-center justify-center">
-                        <Button className="font-semibold text-white bg-[#221C35]" size="md" radius="full">Filter Instagram <FaCaretDown /></Button>
-                    </div>
-                    <div className="flex items-center justify-center">
-                        <Button className="font-semibold text-slate-500" size="md" radius="full">Undangan Cetak <FaCaretDown /></Button>
-                    </div>
-                </div>
+        <div className="p-10">
+            <h1 className="mt-10 text-center text-4xl font-bold">Pilih Paket Terbaikmu</h1>
+            <div className="flex gap-2 items-center flex justify-center mt-[2.5rem]">
+                {list.map((item, index) => (
+                    <Button size="lg" radius="sm"  className={selectedId === item.id ? "bg-[#035B7D] text-white" : "bg-white text-[#035B7D]"} onClick={() => handleButtonClick(item.id)}>
+                        {item.title}
+                    </Button> 
+                ))}
             </div>
 
             <div className="grid grid-cols-2 pt-16">
                 <div>
-                    <h1 className="text-lg font-semibold">Berbagai Pilihan Tema Undangan</h1>
-                </div>
-                <div>
-                    <h1 className="text-lg font-semibold float-right">Beberapa Pilihan Desain Terbaik</h1>
+                    <h1 className="text-lg font-semibold">Pilihlah Design Terbaikmu !</h1>
                 </div>
             </div>
 
@@ -42,8 +56,8 @@ const SectionTwo = () => {
                         <img className="rounded-t-lg" src="https://res.cloudinary.com/practicaldev/image/fetch/s--l4Ne9mZQ--/c_limit%2Cf_auto%2Cfl_progressive%2Cq_100%2Cw_880/https://res.cloudinary.com/practicaldev/image/fetch/s--1uwa-GMK--/c_limit%252Cf_auto%252Cfl_progressive%252Cq_auto%252Cw_350/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/pbqud1ltn46amch1q6jy.jpg" />
                         <div className="p-3 px-5 bg-[#E7F0FF] rounded-b-lg">
                             <h1 className="font-semibold text-lg">Nama Tema</h1>
-                            <Button className="w-full bg-[#035B7D] text-white my-2 mt-5" radius="full" size="sm">Preview Tema <FaArrowRight /></Button>
-                            <Button className="w-full bg-transparent font-bold text-[#035B7D] border-2 border-[#035B7D] my-1" radius="full" size="sm">Masukan Keranjang <MdOutlineShoppingCartCheckout /></Button>
+                            <Button className="w-full bg-[#035B7D] text-white my-2 mt-5" radius="full" size="md">Preview Tema <FaArrowRight /></Button>
+                            <Button className="w-full bg-transparent text-[#035B7D] border-2 border-[#035B7D] my-1" radius="full" size="md">Masukan Keranjang <MdOutlineShoppingCartCheckout /></Button>
                         </div>
                     </div>
                 </div>
@@ -52,8 +66,8 @@ const SectionTwo = () => {
                         <img className="rounded-t-lg" src="https://res.cloudinary.com/practicaldev/image/fetch/s--l4Ne9mZQ--/c_limit%2Cf_auto%2Cfl_progressive%2Cq_100%2Cw_880/https://res.cloudinary.com/practicaldev/image/fetch/s--1uwa-GMK--/c_limit%252Cf_auto%252Cfl_progressive%252Cq_auto%252Cw_350/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/pbqud1ltn46amch1q6jy.jpg" />
                         <div className="p-3 px-5 bg-[#E7F0FF] rounded-b-lg">
                             <h1 className="font-semibold text-lg">Nama Tema</h1>
-                            <Button className="w-full bg-[#035B7D] text-white my-2 mt-5" radius="full" size="sm">Preview Tema <FaArrowRight /></Button>
-                            <Button className="w-full bg-transparent font-bold text-[#035B7D] border-2 border-[#035B7D] my-1" radius="full" size="sm">Masukan Keranjang <MdOutlineShoppingCartCheckout /></Button>
+                            <Button className="w-full bg-[#035B7D] text-white my-2 mt-5" radius="full" size="md">Preview Tema <FaArrowRight /></Button>
+                            <Button className="w-full bg-transparent text-[#035B7D] border-2 border-[#035B7D] my-1" radius="full" size="md">Masukan Keranjang <MdOutlineShoppingCartCheckout /></Button>
                         </div>
                     </div>
                 </div>
@@ -62,8 +76,8 @@ const SectionTwo = () => {
                         <img className="rounded-t-lg" src="https://res.cloudinary.com/practicaldev/image/fetch/s--l4Ne9mZQ--/c_limit%2Cf_auto%2Cfl_progressive%2Cq_100%2Cw_880/https://res.cloudinary.com/practicaldev/image/fetch/s--1uwa-GMK--/c_limit%252Cf_auto%252Cfl_progressive%252Cq_auto%252Cw_350/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/pbqud1ltn46amch1q6jy.jpg" />
                         <div className="p-3 px-5 bg-[#E7F0FF] rounded-b-lg">
                             <h1 className="font-semibold text-lg">Nama Tema</h1>
-                            <Button className="w-full bg-[#035B7D] text-white my-2 mt-5" radius="full" size="sm">Preview Tema <FaArrowRight /></Button>
-                            <Button className="w-full bg-transparent font-bold text-[#035B7D] border-2 border-[#035B7D] my-1" radius="full" size="sm">Masukan Keranjang <MdOutlineShoppingCartCheckout /></Button>
+                            <Button className="w-full bg-[#035B7D] text-white my-2 mt-5" radius="full" size="md">Preview Tema <FaArrowRight /></Button>
+                            <Button className="w-full bg-transparent text-[#035B7D] border-2 border-[#035B7D] my-1" radius="full" size="md">Masukan Keranjang <MdOutlineShoppingCartCheckout /></Button>
                         </div>
                     </div>
                 </div>
@@ -72,8 +86,8 @@ const SectionTwo = () => {
                         <img className="rounded-t-lg" src="https://res.cloudinary.com/practicaldev/image/fetch/s--l4Ne9mZQ--/c_limit%2Cf_auto%2Cfl_progressive%2Cq_100%2Cw_880/https://res.cloudinary.com/practicaldev/image/fetch/s--1uwa-GMK--/c_limit%252Cf_auto%252Cfl_progressive%252Cq_auto%252Cw_350/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/pbqud1ltn46amch1q6jy.jpg" />
                         <div className="p-3 px-5 bg-[#E7F0FF] rounded-b-lg">
                             <h1 className="font-semibold text-lg">Nama Tema</h1>
-                            <Button className="w-full bg-[#035B7D] text-white my-2 mt-5" radius="full" size="sm">Preview Tema <FaArrowRight /></Button>
-                            <Button className="w-full bg-transparent font-bold text-[#035B7D] border-2 border-[#035B7D] my-1" radius="full" size="sm">Masukan Keranjang <MdOutlineShoppingCartCheckout /></Button>
+                            <Button className="w-full bg-[#035B7D] text-white my-2 mt-5" radius="full" size="md">Preview Tema <FaArrowRight /></Button>
+                            <Button className="w-full bg-transparent text-[#035B7D] border-2 border-[#035B7D] my-1" radius="full" size="md">Masukan Keranjang <MdOutlineShoppingCartCheckout /></Button>
                         </div>
                     </div>
                 </div>
@@ -84,8 +98,8 @@ const SectionTwo = () => {
                         <img className="rounded-t-lg" src="https://res.cloudinary.com/practicaldev/image/fetch/s--l4Ne9mZQ--/c_limit%2Cf_auto%2Cfl_progressive%2Cq_100%2Cw_880/https://res.cloudinary.com/practicaldev/image/fetch/s--1uwa-GMK--/c_limit%252Cf_auto%252Cfl_progressive%252Cq_auto%252Cw_350/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/pbqud1ltn46amch1q6jy.jpg" />
                         <div className="p-3 px-5 bg-[#E7F0FF] rounded-b-lg">
                             <h1 className="font-semibold text-lg">Nama Tema</h1>
-                            <Button className="w-full bg-[#035B7D] text-white my-2 mt-5" radius="full" size="sm">Preview Tema <FaArrowRight /></Button>
-                            <Button className="w-full bg-transparent font-bold text-[#035B7D] border-2 border-[#035B7D] my-1" radius="full" size="sm">Masukan Keranjang <MdOutlineShoppingCartCheckout /></Button>
+                            <Button className="w-full bg-[#035B7D] text-white my-2 mt-5" radius="full" size="md">Preview Tema <FaArrowRight /></Button>
+                            <Button className="w-full bg-transparent text-[#035B7D] border-2 border-[#035B7D] my-1" radius="full" size="md">Masukan Keranjang <MdOutlineShoppingCartCheckout /></Button>
                         </div>
                     </div>
                 </div>
@@ -94,8 +108,8 @@ const SectionTwo = () => {
                         <img className="rounded-t-lg" src="https://res.cloudinary.com/practicaldev/image/fetch/s--l4Ne9mZQ--/c_limit%2Cf_auto%2Cfl_progressive%2Cq_100%2Cw_880/https://res.cloudinary.com/practicaldev/image/fetch/s--1uwa-GMK--/c_limit%252Cf_auto%252Cfl_progressive%252Cq_auto%252Cw_350/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/pbqud1ltn46amch1q6jy.jpg" />
                         <div className="p-3 px-5 bg-[#E7F0FF] rounded-b-lg">
                             <h1 className="font-semibold text-lg">Nama Tema</h1>
-                            <Button className="w-full bg-[#035B7D] text-white my-2 mt-5" radius="full" size="sm">Preview Tema <FaArrowRight /></Button>
-                            <Button className="w-full bg-transparent font-bold text-[#035B7D] border-2 border-[#035B7D] my-1" radius="full" size="sm">Masukan Keranjang <MdOutlineShoppingCartCheckout /></Button>
+                            <Button className="w-full bg-[#035B7D] text-white my-2 mt-5" radius="full" size="md">Preview Tema <FaArrowRight /></Button>
+                            <Button className="w-full bg-transparent text-[#035B7D] border-2 border-[#035B7D] my-1" radius="full" size="md">Masukan Keranjang <MdOutlineShoppingCartCheckout /></Button>
                         </div>
                     </div>
                 </div>
@@ -104,8 +118,8 @@ const SectionTwo = () => {
                         <img className="rounded-t-lg" src="https://res.cloudinary.com/practicaldev/image/fetch/s--l4Ne9mZQ--/c_limit%2Cf_auto%2Cfl_progressive%2Cq_100%2Cw_880/https://res.cloudinary.com/practicaldev/image/fetch/s--1uwa-GMK--/c_limit%252Cf_auto%252Cfl_progressive%252Cq_auto%252Cw_350/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/pbqud1ltn46amch1q6jy.jpg" />
                         <div className="p-3 px-5 bg-[#E7F0FF] rounded-b-lg">
                             <h1 className="font-semibold text-lg">Nama Tema</h1>
-                            <Button className="w-full bg-[#035B7D] text-white my-2 mt-5" radius="full" size="sm">Preview Tema <FaArrowRight /></Button>
-                            <Button className="w-full bg-transparent font-bold text-[#035B7D] border-2 border-[#035B7D] my-1" radius="full" size="sm">Masukan Keranjang <MdOutlineShoppingCartCheckout /></Button>
+                            <Button className="w-full bg-[#035B7D] text-white my-2 mt-5" radius="full" size="md">Preview Tema <FaArrowRight /></Button>
+                            <Button className="w-full bg-transparent text-[#035B7D] border-2 border-[#035B7D] my-1" radius="full" size="md">Masukan Keranjang <MdOutlineShoppingCartCheckout /></Button>
                         </div>
                     </div>
                 </div>
@@ -114,8 +128,8 @@ const SectionTwo = () => {
                         <img className="rounded-t-lg" src="https://res.cloudinary.com/practicaldev/image/fetch/s--l4Ne9mZQ--/c_limit%2Cf_auto%2Cfl_progressive%2Cq_100%2Cw_880/https://res.cloudinary.com/practicaldev/image/fetch/s--1uwa-GMK--/c_limit%252Cf_auto%252Cfl_progressive%252Cq_auto%252Cw_350/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/pbqud1ltn46amch1q6jy.jpg" />
                         <div className="p-3 px-5 bg-[#E7F0FF] rounded-b-lg">
                             <h1 className="font-semibold text-lg">Nama Tema</h1>
-                            <Button className="w-full bg-[#035B7D] text-white my-2 mt-5" radius="full" size="sm">Preview Tema <FaArrowRight /></Button>
-                            <Button className="w-full bg-transparent font-bold text-[#035B7D] border-2 border-[#035B7D] my-1" radius="full" size="sm">Masukan Keranjang <MdOutlineShoppingCartCheckout /></Button>
+                            <Button className="w-full bg-[#035B7D] text-white my-2 mt-5" radius="full" size="md">Preview Tema <FaArrowRight /></Button>
+                            <Button className="w-full bg-transparent text-[#035B7D] border-2 border-[#035B7D] my-1" radius="full" size="md">Masukan Keranjang <MdOutlineShoppingCartCheckout /></Button>
                         </div>
                     </div>
                 </div>
