84203e3359059f57baf24738857801cb203e67d4
= <=>
2024-02-11 22:21:05 +0700

    new update

--- app/home/section1.js
+++ app/home/section1.js
@@ -1,55 +1,68 @@
-'use client'
+"use client";
 import styles from "../home/css/section1Home.module.css";
 
 import { FaWhatsapp } from "react-icons/fa6";
-import Typewriter from 'typewriter-effect';
+import Typewriter from "typewriter-effect";
 import { Button } from "@nextui-org/react";
 
-
 const SectionOne = () => {
-
-    const text = () => {
-        useTypewriter({
-            words: ['Pernikahan?', 'Tunangan?'],
-            loop: true,
-        })
-    } 
-    return (
-        <div className={`${styles.section1} container mx-auto px-5 py-28 sm:grid sm:grid-cols-1 md:grid-cols-1 lg:grid lg:grid-cols-2 xl:grid xl:grid-cols-2`}>
-            <div className='p-4 flex items-center justify-center'>
-                <div className='lg:text-left xl:text-left xl:ml-28 text-center '>
-                    <h1 className={`${styles.title} text-2xl lg:text-[3rem] lg:leading-[4rem] leading-[1] font-black mb-2 lg:mb-0`}>
-                        Buat Undangan Online
-                        <br className="hidden lg:inline" /> {/* Hanya tampil di atas lg (large) */}
-                        Digital Website Untuk
-                        <br className="hidden lg:inline" /> {/* Hanya tampil di atas lg (large) */}
-                        <span>
-                            <Typewriter 
-                                options={{
-                                    strings: [
-                                        'Pernikahan?',
-                                        'Tunangan?',
-                                    ],
-                                    autoStart: true,
-                                    loop: true
-                                }}
-                            />
-                        </span>
-                    </h1>
-                    <p className={`${styles.description} text-lg lg:text-2l leading-[1.6rem] py-5 mb-5 lg:mb-0`}>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi corrupti ea repudiandae inventore eveniet veniam excepturi, autem dolorum omnis. Exercitationem et explicabo repellendus eaque assumenda eum blanditiis excepturi aspernatur voluptates?
-                    </p>
-                    {/* Menyembunyikan button pada mode mobile */}
-                    <Button className={`${styles.button} mt-3 bg-transparent hidden sm:block lg:flex xl:flex`} radius="full" size="md">
-                        Konsultasi Gratis <FaWhatsapp />
-                    </Button>
-                </div>
-            </div>
-            <div>
-                <img className='block mx-auto w-3/5 xl:w-3/5 lg:w-5/6' src="/image/home/section1/section1Home.png" />
-            </div>
+  const text = () => {
+    useTypewriter({
+      words: ["Pernikahan?", "Tunangan?"],
+      loop: true,
+    });
+  };
+  return (
+    <div
+      className={`${styles.section1} container mx-auto px-5 py-28 sm:grid sm:grid-cols-1 md:grid-cols-1 lg:grid lg:grid-cols-2 xl:grid xl:grid-cols-2`}
+    >
+      <div className="p-4 flex items-center justify-center">
+        <div className="lg:text-left xl:text-left xl:ml-28 text-center ">
+          <h1
+            className={`${styles.title} text-2xl lg:text-[3rem] lg:leading-[4rem] leading-[1] font-black mb-2 lg:mb-0`}
+          >
+            Buat Undangan Online
+            <br className="hidden lg:inline" />{" "}
+            {/* Hanya tampil di atas lg (large) */}
+            Digital Website Untuk
+            <br className="hidden lg:inline" />{" "}
+            {/* Hanya tampil di atas lg (large) */}
+            <span>
+              <Typewriter
+                options={{
+                  strings: ["Pernikahan?", "Tunangan?"],
+                  autoStart: true,
+                  loop: true,
+                }}
+              />
+            </span>
+          </h1>
+          <p
+            className={`${styles.description} text-lg lg:text-2l leading-[1.6rem] py-5 mb-5 lg:mb-0`}
+          >
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi
+            corrupti ea repudiandae inventore eveniet veniam excepturi, autem
+            dolorum omnis. Exercitationem et explicabo repellendus eaque
+            assumenda eum blanditiis excepturi aspernatur voluptates?
+          </p>
+          {/* Menyembunyikan button pada mode mobile */}
+          <Button
+            className={`${styles.button} mt-3 bg-transparent hidden sm:block lg:flex xl:flex`}
+            radius="full"
+            size="md"
+          >
+            Konsultasi Gratis <FaWhatsapp />
+          </Button>
         </div>
-    )
-}
+      </div>
+      <div>
+        <img
+          className="block mx-auto w-3/5 xl:w-6/5 "
+          src="/image/home/section1/section-1.png"
+        />
+      </div>
+    </div>
+  );
+};
 
 export default SectionOne;
--- app/home/section2.js
+++ app/home/section2.js
@@ -1,42 +1,54 @@
 import React from "react";
-import { Card, CardHeader, CardBody, Image, CardFooter, Button } from "@nextui-org/react";
+import {
+  Card,
+  CardHeader,
+  CardBody,
+  Image,
+  CardFooter,
+  Button,
+} from "@nextui-org/react";
 import { FaArrowRight } from "react-icons/fa6";
 import styles from "../home/css/section2Home.module.css";
 
 const SectionTwo = () => {
-
   const list = [
     {
       title: "Undangan Digital",
-      img: "/images/fruit-1.jpeg",
-      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+      img: "/image/home/section2/card-digital-invitation.png",
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
     },
     {
       title: "Undangan 3D",
-      img: "/images/fruit-2.jpeg",
-      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+      img: "/image/home/section2/card-invitation-3d-grayscale.png",
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
     },
     {
       title: "Filter Instagram",
-      img: "/images/fruit-3.jpeg",
-      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+      img: "/image/home/section2/card-instagram-filter.png",
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
     },
     {
       title: "Undangan Cetak",
-      img: "/images/fruit-4.jpeg",
-      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+      img: "/image/home/section2/card-print-invitation-grayscale.png",
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
     },
   ];
 
   return (
     <>
-    <div className="container mx-auto">
-      <div className="grid grid-cols-1 px-10 xl:mt-12 lg:mt-12">
-        <div className="lg:container lg:w-26">
-          <h1 className="text-center xl:text-3xl lg:text-3xl xl:p-0 lg:p-0 text-3xl py-7 font-extrabold">Semua yang kamu perlukan untuk bantu wujudkan Momen Impian !</h1>
+      <div className="container mx-auto">
+        <div className="grid grid-cols-1 px-10 xl:mt-12 lg:mt-12">
+          <div className="lg:container lg:w-26">
+            <h1 className="text-center xl:text-3xl lg:text-3xl xl:p-0 lg:p-0 text-3xl py-7 font-extrabold">
+              Semua yang kamu perlukan untuk bantu wujudkan Momen Impian !
+            </h1>
+          </div>
         </div>
       </div>
-    </div>
 
       <div className="container mx-auto p-10 py-15 xl:mt-6 lg:mt-6">
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-3">
@@ -47,26 +59,32 @@ const SectionTwo = () => {
                   isZoomed
                   alt="Card background"
                   className="object-cover rounded-xl block mx-auto w-full"
-                  src="https://res.cloudinary.com/practicaldev/image/fetch/s--l4Ne9mZQ--/c_limit%2Cf_auto%2Cfl_progressive%2Cq_100%2Cw_880/https://res.cloudinary.com/practicaldev/image/fetch/s--1uwa-GMK--/c_limit%252Cf_auto%252Cfl_progressive%252Cq_auto%252Cw_350/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/pbqud1ltn46amch1q6jy.jpg"
+                  src={`${item.img}`}
                 />
               </CardBody>
               <CardFooter className="pb-0 pt-2 px-4 flex-col items-start">
-                <h4 className={`${styles.titleColor} font-bold text-large`}>{item.title}</h4>
-                <p className={`${styles.descriptionCardColor} mt-2 text-sm font-bold`}>{item.description}</p>
-                <Button className={`${styles.descriptionCardColor} mt-4 bg-transparent`} radius="full" size="md">Lihat lebih lanjut <FaArrowRight /></Button>
+                <h4 className={`${styles.titleColor} font-bold text-large`}>
+                  {item.title}
+                </h4>
+                <p
+                  className={`${styles.descriptionCardColor} mt-2 text-sm font-bold`}
+                >
+                  {item.description}
+                </p>
+                <Button
+                  className={`${styles.descriptionCardColor} mt-4 bg-transparent`}
+                  radius="full"
+                  size="md"
+                >
+                  Lihat lebih lanjut <FaArrowRight />
+                </Button>
               </CardFooter>
             </Card>
           ))}
         </div>
       </div>
-
-
-
-
-
-
     </>
-  )
-}
+  );
+};
 
 export default SectionTwo;
